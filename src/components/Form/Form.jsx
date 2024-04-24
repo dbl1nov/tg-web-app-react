51963684a1a4568e56ba88c8ae4b55afb5d0dc74
Daniil <69772348+dbl1nov@users.noreply.github.com>
2024-04-24 16:34:21 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -20,7 +20,7 @@ const Form = () => {
         }else{
             tg.MainButton.show();
         }
-    }, [tg,street,country]);
+    }, [country, street, tg]);
 
 
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,6 @@ const Header = () => {
         </span>
     </div>
 )
-    ;
 };
 
 export default Header;
