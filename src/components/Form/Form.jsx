4b23c09f1f3a18b0cfe026ffe5a63b3a677710b6
Daniil <69772348+dbl1nov@users.noreply.github.com>
2024-04-25 14:05:30 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,11 +18,11 @@ const Form = () => {
     }, [country, street, subject, tg])
 
     useEffect(() => {
-        tg.WebApp.onEvent('mainButtonClicked', onSendData)
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.WebApp.offEvent('mainButtonClicked', onSendData)
+            tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [onSendData, tg]);
+    }, [onSendData]);
 
 
     useEffect(() => {
